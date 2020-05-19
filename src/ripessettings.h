61eb4fdedf8b3c4a5d37457ac7cd24ac2e2166ff
Morten Borup Petersen <morten_bp@live.dk>
2020-05-19 17:15:54 +0200

    Add settings for restoring state between executions

--- src/ripessettings.h
+++ src/ripessettings.h
@@ -5,7 +5,8 @@
 
 namespace Ripes {
 
-// Definitions of the name of all settings within Ripes
+// =========== Definitions of the name of all settings within Ripes ============
+// User-modifyable settings
 #define RIPES_SETTING_REWINDSTACKSIZE ("simulator_rewindstacksize")
 #define RIPES_SETTING_CCPATH ("compiler_path")
 #define RIPES_SETTING_CCARGS ("compiler_args")
@@ -13,15 +14,31 @@ namespace Ripes {
 #define RIPES_SETTING_CONSOLEBG ("console_bg_color")
 #define RIPES_SETTING_CONSOLEFONT ("console_font_color")
 
-// Definitions of all default settings within Ripes
+// Program state preserving settings
+#define RIPES_SETTING_APP_TAB ("app_tab")
+#define RIPES_SETTING_SETTING_TAB ("settings_tab")
+#define RIPES_SETTING_VIEW_ZOOM ("view_zoom")
+#define RIPES_SETTING_PROCESSOR_ID ("processor_id")
+#define RIPES_SETTING_PROCESSOR_LAYOUT_ID ("processor_layout_id")
+#define RIPES_SETTING_FOLLOW_EXEC ("follow_execution")
+
+// ============= Definitions of all default settings within Ripes ==============
 const static std::map<QString, QVariant> s_defaultSettings = {
+    // User-modifyable settings
     {RIPES_SETTING_REWINDSTACKSIZE, 100},
     {RIPES_SETTING_CCPATH, ""},
     {RIPES_SETTING_CCARGS, "-O0"},
     {RIPES_SETTING_CONSOLEECHO, "true"},
     {RIPES_SETTING_CONSOLEBG, QColor(Qt::white)},
     {RIPES_SETTING_CONSOLEFONT, QColor(Qt::black)},
-};
+
+    // Program state preserving settings
+    {RIPES_SETTING_APP_TAB, 1 /* Processor */},
+    {RIPES_SETTING_SETTING_TAB, 0},
+    {RIPES_SETTING_VIEW_ZOOM, 250},
+    {RIPES_SETTING_PROCESSOR_ID, QVariant() /* Let processorhandler define default */},
+    {RIPES_SETTING_PROCESSOR_LAYOUT_ID, 0},
+    {RIPES_SETTING_FOLLOW_EXEC, "true"}};
 
 /**
  * @brief The SettingObserver class
